8ca856304511071fbb8801fdc0420ba506e4d7be
Varun <varun@qubicle.co.in>
2022-06-16 21:13:18 +0530

    chatbox html

--- app/src/containers/HomePage/index.js
+++ app/src/containers/HomePage/index.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React from "react";
+import "./home.css";
+import ChatBox from "../../components/ChatBox";
 
 export default function index() {
-  return (
-    <div>Home</div>
-  )
+  return <ChatBox />;
 }
